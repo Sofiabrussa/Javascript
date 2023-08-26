2315ab2b3dbd554a3b55f7fd9b0a224c23add522
sofiabrussa <sofiabrussa@gmail.com>
2023-08-26 13:46:13 -0300

    listo

--- ejercicios.js
+++ ejercicios.js
@@ -1,6 +1,5 @@
 
 // Escribí un programa que dado un año como entrada diga si es bisiesto o no.
-
 let año = 1924;
 
 if ((año % 4 === 0 && año % 100 !== 0) || (año % 400 === 0)) {
@@ -9,4 +8,12 @@ if ((año % 4 === 0 && año % 100 !== 0) || (año % 400 === 0)) {
 else  {
     console.log(año + " No es bisiesto")
 }
+
+
+
+console.log("listo")
+
+
+
+
 
